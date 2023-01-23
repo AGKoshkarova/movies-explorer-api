5514362bd111b7e250fe6f8efdad58071d880dcc
Anna Koshkarova <koshkarova.anya@yandex.ru>
2023-01-23 14:12:24 +0300

    refactor: вынести роуты в общий файл, очистить app.js

--- app.js
+++ app.js
@@ -2,24 +2,19 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-// const { celebrate, Joi } = require('celebrate');
+const helmet = require('helmet');
+const { errors } = require('celebrate');
 
-const authRouter = require('./routes/authRouter');
-const userRouter = require('./routes/users');
-const movieRouter = require('./routes/movies');
+const router = require('./routes/index');
 
-const { checkAuth } = require('./middlewares/auth');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-
-const NotFoundError = require('./errors/not-found-err');
-
-const { MESSAGE_404 } = require('./utils/constants');
-
-// const { createUser, login } = require('./controllers/users');
+const { findErrors } = require('./middlewares/errors');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+app.use(helmet());
 
 app.use(cookieParser());
 
@@ -31,36 +26,15 @@ mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
 
 app.use(requestLogger);
 
-// запросы на регистрацию и авторизацию
-app.use(authRouter);
-
-// запрос на выход
-app.get('/signout', (req, res) => {
-  res.clearCookie('jwt').json({ message: 'Куки очищены' });
-});
-
-// защта роутов авторизацией
-app.use(checkAuth);
-
-app.use(userRouter);
-
-app.use(movieRouter);
-
-app.use('*', (req, res, next) => {
-  next(new NotFoundError(MESSAGE_404));
-});
+// подключение роутов
+app.use(router);
 
 app.use(errorLogger);
 
-app.use((err, req, res, next) => {
-  const { statusCode = 500, message } = err;
+app.use(errors());
 
-  res.status(statusCode).send({
-    message: statusCode === 500 ? 'На сервере произошла ошибка' : message,
-  });
-
-  next();
-});
+// централизованный обработчик ошибок
+app.use(findErrors);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
---
+++ routes/index.js
@@ -0,0 +1,41 @@
+const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+
+const { createUser, login } = require('../controllers/users');
+
+const { checkAuth } = require('../middlewares/auth');
+
+const userRouter = require('./users');
+const movieRouter = require('./movies');
+
+const NotFoundError = require('../errors/not-found-err');
+const { MESSAGE_404 } = require('../utils/constants');
+
+router.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+  }),
+}), createUser);
+
+router.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+  }),
+}), login);
+
+router.get('/signout', (req, res) => {
+  res.clearCookie('jwt').json({ message: 'Куки очищены' });
+});
+
+router.use(checkAuth, userRouter);
+
+router.use(checkAuth, movieRouter);
+
+router.use('*', (req, res, next) => {
+  next(new NotFoundError(MESSAGE_404));
+});
+
+module.exports = router;
