179c37458e5026cf38d0d3011a8fe3106a645b6b
Anna Koshkarova <koshkarova.anya@yandex.ru>
2023-01-24 17:54:35 +0300

    feat: настроить CORS

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
+const cors = require('cors');
 
 const router = require('./routes/index');
 
@@ -11,6 +12,18 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { findErrors } = require('./middlewares/errors');
 
 const { PORT = 3000 } = process.env;
+
+const allowedCors = [
+  'localhost:3000',
+  'https://diploma.koshkarova.nomoredomains.rocks',
+  'https://api.diploma.koshkarova.nomoredomains.rocks',
+];
+
+const corsOptions = {
+  origin: allowedCors,
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
 
 const app = express();
 
@@ -25,6 +38,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
 
 app.use(requestLogger);
+
+// подключаем CORS
+app.use(cors(corsOptions));
 
 // подключение роутов
 app.use(router);
